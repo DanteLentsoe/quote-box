ba9692a2fb59dc6e748a730ca0c0d895ff74d9ff
Dante L. Lentsoe <65385487+DanteLentsoe@users.noreply.github.com>
2020-08-18 09:00:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+/* Formation of quotes of arrays */
+//const quoteArray = [{"quote":"To hope is to stare into the face of death, waiting for life, hope is irrational.","author":"Dante Lentsoe"},
+                   // {"quote":"Fear of death is naive, we lay our heads to rest our minds everyday, to glimpse on our eventual inevitable death, we fear it. Yet, we only need to perform the act once, that we practised for our whole lives"}, "author":"Dante Lentsoe",
+                   //{"quote":""}]
+const MyGlobal = () =>
+(<h1> This Will Be the first of many </h1>);
+
+ReactDOM.render(<MyGlobal />, 
+  document.getElementById('Application')
+);
+
+//serviceWorker.unregister();
